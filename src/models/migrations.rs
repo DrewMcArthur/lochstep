ab326fd38949b922780a5ceb21fb2af1f14b31dd
drewmcarthur <drewmcarthur1@gmail.com>
2023-08-06 21:35:05 -0400

    refactor migration function

--- src/models/migrations.rs
+++ src/models/migrations.rs
@@ -3,40 +3,14 @@ use libsql_client::{args, Statement};
 
 use crate::errors::Errors;
 
-static GET_LATEST_MIGRATION: &str = "SELECT id FROM migrations ORDER BY id DESC LIMIT 1;";
-static ADD_MIGRATION: &str = "INSERT INTO migrations (id, date, query) VALUES (?, ?, ?);";
-
-static CREATE_MIGRATIONS_TABLE: &str = "CREATE TABLE IF NOT EXISTS migrations (
-        id INT PRIMARY KEY,
-        date TEXT,
-        query TEXT
-    );";
-static CREATE_USERS_TABLE: &str = "CREATE TABLE IF NOT EXISTS users (
-        id TEXT PRIMARY KEY,
-        username TEXT UNIQUE,
-        hash TEXT,
-        salt TEXT
-        );";
-static CREATE_KEYS_TABLE: &str = "CREATE TABLE IF NOT EXISTS keys (
-        id INT PRIMARY KEY,
-        userid TEXT,
-        key TEXT
-        );";
-static CREATE_PROPOSALS_TABLE: &str = "CREATE TABLE IF NOT EXISTS proposals (
-        id INT PRIMARY KEY,
-        title TEXT,
-        description TEXT,
-        authorId TEXT,
-        createdAt TEXT,
-        updatedAt TEXT
-    );";
+mod queries;
 
 // this array should only ever be added to; never changed
 pub static MIGRATIONS: [&str; 4] = [
-    CREATE_MIGRATIONS_TABLE,
-    CREATE_USERS_TABLE,
-    CREATE_KEYS_TABLE,
-    CREATE_PROPOSALS_TABLE,
+    queries::CREATE_MIGRATIONS_TABLE,
+    queries::CREATE_USERS_TABLE,
+    queries::CREATE_KEYS_TABLE,
+    queries::CREATE_PROPOSALS_TABLE,
 ];
 
 pub async fn migrate_db(
@@ -44,21 +18,18 @@ pub async fn migrate_db(
     migrations: &Vec<&str>,
 ) -> Result<usize, Errors> {
     let mut migrations_executed: usize = 0;
-    let latest_migration: usize = get_latest(client).await.unwrap_or(0);
+    let latest_migration = get_latest(client).await;
+    let mut i = latest_migration.unwrap_or(0);
 
-    for (i, query) in migrations.iter().enumerate() {
-        if i >= latest_migration {
-            client
-                .execute(query)
-                .await
-                .unwrap_or_else(|e| panic!("error initializing db on query {i}: {e}"));
+    while i < migrations.len() {
+        let query = migrations[i];
+        exec_migration(client, query).await.unwrap();
+        add(client, i + 1, query)
+            .await
+            .expect("error updating migrations table");
 
-            add(client, i + 1, query)
-                .await
-                .expect("error updating migrations table");
-
-            migrations_executed += 1;
-        }
+        i += 1;
+        migrations_executed += 1;
     }
 
     Ok(migrations_executed)
@@ -66,7 +37,7 @@ pub async fn migrate_db(
 
 async fn get_latest(client: &libsql_client::Client) -> Result<usize, Errors> {
     client
-        .execute(GET_LATEST_MIGRATION)
+        .execute(queries::GET_LATEST_MIGRATION)
         .await
         .map(|rs: libsql_client::ResultSet| {
             rs.rows
@@ -77,9 +48,18 @@ async fn get_latest(client: &libsql_client::Client) -> Result<usize, Errors> {
         .map_err(|e| Errors::DbFetchLatestMigrationError(e))
 }
 
+async fn exec_migration(client: &libsql_client::Client, query: &str) -> Result<(), Errors> {
+    client
+        .execute(query)
+        .await
+        .unwrap_or_else(|e| panic!("error initializing db on query {query}: {e}"));
+
+    Ok(())
+}
+
 async fn add(client: &libsql_client::Client, id: usize, query: &str) -> Result<(), Errors> {
     let now = chrono::offset::Utc::now().to_rfc3339();
-    let stmt = Statement::with_args(ADD_MIGRATION, args![id, now, query]);
+    let stmt = Statement::with_args(queries::ADD_MIGRATION, args![id, now, query]);
     client
         .execute(stmt)
         .await
